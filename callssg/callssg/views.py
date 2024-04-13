006d192409e4ce852e71594c36cb8b3bc7d565d9
kevin <kevsamaniego60@gmail.com>
2024-04-13 10:17:59 -0400

    La funcionalidad de notas ya funciona correctamente, guarda, actualiza y muestra los datos correctamente

--- callssg/callssg/views.py
+++ callssg/callssg/views.py
@@ -8,22 +8,22 @@ from django.shortcuts import get_object_or_404
 def clientes_view(request):
     # Obtener todos los clientes de la base de datos
     clientes = Cliente.objects.all().order_by('cedula')
-    
+
     # Paginar los clientes (por ejemplo, x clientes por página)
     paginator = Paginator(clientes, 1)
-    
+
     # Obtener el número de página de la URL (si no se especifica, será la primera página)
     page_number = request.GET.get('page')
-    
+
     # Obtener la página actual
     page_obj = paginator.get_page(page_number)
-    
+
     # Obtener la cédula del cliente actual, si no se proporciona, tomar el primer cliente de la base de datos
     cliente_id = request.POST.get('cliente')
     if not cliente_id:
         primer_cliente = clientes.first()
         if not primer_cliente:
-            return HttpResponse("No hay clientes en la base de datos")  # O maneja de alguna otra manera si no hay clientes
+            return HttpResponse("No hay clientes en la base de datos")
         cliente_id = primer_cliente.cedula
 
     # Obtener el cliente actual
@@ -31,9 +31,6 @@ def clientes_view(request):
 
     # Obtener la última nota del cliente
     ultima_nota = cliente.notas_set.last()
-
-    # Inicializar nota_actualizada con un valor predeterminado
-    nota_actualizada = ultima_nota
 
     if request.method == 'POST':
         form = NotasForm(request.POST)
@@ -43,18 +40,18 @@ def clientes_view(request):
                 # Si ya hay una nota para este cliente, actualízala
                 ultima_nota.texto = nota_texto
                 ultima_nota.save()
-                nota_actualizada = ultima_nota  # Obtener la nota actualizada
+                nota_actualizada = ultima_nota
             else:
                 # Si no hay una nota para este cliente, crea una nueva
                 nueva_nota = Notas(cliente=cliente, texto=nota_texto)
                 nueva_nota.save()
-                nota_actualizada = nueva_nota  # Obtener la nota recién creada
-            
+                nota_actualizada = nueva_nota
     else:
-        form = NotasForm(initial={'cliente': cliente_id})  # Inicializa el formulario con el ID del cliente
-    
+        form = NotasForm(initial={'cliente': cliente_id})
+        nota_actualizada = ultima_nota  # Asignar ultima_nota a nota_actualizada si no es una solicitud POST
+
     # Renderizar la plantilla con los datos paginados y el formulario de notas
-    context = {'page_obj': page_obj, 'form': form, 'ultima_nota': nota_actualizada}
+    context = {'page_obj': page_obj, 'form': form, 'ultima_nota': ultima_nota}
     return render(request, 'cliente.html', context)
 
 """
